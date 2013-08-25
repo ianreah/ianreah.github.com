c8525d541146dfa7dde827d671e8da63e6bef6fc
Ian Reah <ian.reah@gmail.com>
2013-08-25 12:08:32 +0100

    Adding some folders in preparing for the redesign

---
+++ js/index.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var $toggleLink = $("#toggleLink");
+    $("#moreProjects").bind('shown', function () {
+        var topleft = $(this).offset();
+
+        $('html, body').animate({
+            scrollTop: topleft.top,
+            scrollLeft: topleft.left
+        });
+
+        $toggleLink.html("\u00ab See fewer projects");
+    })
+    .bind('hidden', function () {
+        $toggleLink.html("See more projects \u00bb");
+    });
+});
